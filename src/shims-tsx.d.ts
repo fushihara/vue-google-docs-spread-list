5bb87120136d21d7594ea2804dfce73c7deb20df
Fushihara <fushihara@users.noreply.github.com>
2019-04-27 17:01:25 +0900

    d.ts整理

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -1,11 +1,10 @@
-import Vue, { VNode } from 'vue'
-
 declare global {
   export namespace chrome {
     export class identity {
       public static getAuthToken(option?: { interactive: boolean }, callback?: (token: string) => void): void;
     }
   }
+  
 }
 export type SortType = "last_view_me" | "last_update_me" | "last_update" | "createdTime" | "title";
 export type ListItem = {
